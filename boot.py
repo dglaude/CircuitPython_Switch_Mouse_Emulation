3b2c6b5404406dac1f70d5818ea18ce8b3c93572
David Glaude <dglaude@users.noreply.github.com>
2022-04-20 16:40:34 +0200

    Create boot.py

---
+++ boot.py
@@ -0,0 +1,21 @@
+import storage, usb_cdc
+import board, digitalio
+import usb_hid, usb_midi
+
+# On the Macropad, pressing a key grounds it. You need to set a pull-up.
+# If not pressed, the key will be at +V (due to the pull-up).
+button = digitalio.DigitalInOut(board.KEY12)
+button.pull = digitalio.Pull.UP
+
+# Disable devices only if button is not pressed.
+if button.value:
+    storage.disable_usb_drive()
+    print("storage.disable_usb_drive()")
+    usb_cdc.disable()
+    print("usb_cdc.disable()")
+    usb_midi.disable()
+    print("usb_midi.disable()")
+    usb_hid.enable()
+    print("usb_hid.enable()")
+
+print("End of boot.py")
